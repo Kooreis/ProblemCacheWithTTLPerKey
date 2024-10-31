3cd616b9636286bbc813c6a78791f0a2a18455a0
Kooreis <kooreis.official@gmail.com>
2024-10-31 00:51:05 -0500

    This update adds the Add and Get methods to the Cache class. The Add method adds a key-value pair to the cache with a specified TTL. The Get method retrieves a value from the cache using a key.

--- solution.cs
+++ solution.cs
@@ -1,7 +1,20 @@
-using System;
-using System.Collections.Concurrent;
-
-public class Cache<TKey, TValue>
+public void Add(TKey key, TValue value, TimeSpan ttl)
 {
-    private ConcurrentDictionary<TKey, CacheItem> _cache = new ConcurrentDictionary<TKey, CacheItem>();
+    if (_cache.ContainsKey(key))
+    {
+        _cache[key].CancelToken();
+    }
+
+    var item = new CacheItem(value, ttl, () => _cache.TryRemove(key, out _));
+    _cache[key] = item;
+}
+
+public TValue Get(TKey key)
+{
+    if (_cache.TryGetValue(key, out var item))
+    {
+        return item.Value;
+    }
+
+    throw new ArgumentException("No item with the provided key found in the cache.");
 }
